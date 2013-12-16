f5c85594813b65a4ccf60b3aef99fa65442f7702
matteo <mttsrt@gmail.com>
2013-12-16 09:59:47 +0100

    added CRC8 class for handling error checking code. various .classpath, .project, .gitignore

--- aside-code/Client.java
+++ aside-code/Client.java
@@ -1,4 +1,3 @@
-package test;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
--- aside-code/Messages.java
+++ aside-code/Messages.java
@@ -1,4 +1,4 @@
-package app;
+import app.Node;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
