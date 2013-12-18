6023bf578bbdd909be2c22f90191a38c850e6ce9
Raffaele Maffei <maffeira@dei.unipd.it>
2013-12-18 12:15:24 +0100

    Added Cache class

---
+++ aside-code/Cache.java
@@ -0,0 +1,73 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+
+public class Cache {
+	
+	// Fields
+	private static ArrayList<Row> table = new ArrayList<Row>();
+	
+	// Methods
+	public static boolean isThere(InetAddress ip, byte seq) {
+		boolean sw = false;
+		Row r = new Row(ip, seq);
+		int index = searchByIP(ip);
+		
+		if (index == -1)
+			table.add(r);
+		else {
+			Row d = table.remove(index);
+			if(d.getSeq() == seq) {
+				sw = true;
+				table.add(r);
+			}
+		}
+		return sw;
+	}
+	
+	private static int searchByIP(InetAddress ip) {
+		int index = -1;
+		
+		for(int i = 0; i < table.size(); i++) {
+			if(table.get(i).getAddr() == ip) {
+				index = i;
+				break;
+			}
+		}
+		return index;	
+	}
+	
+	public static void print(){
+		//TODO
+		System.out.println();
+	}
+	
+	// --- PRIVATE CLASS ---
+	private static class Row {
+		
+		private InetAddress addr;
+		private byte seq;
+		
+		public Row(InetAddress a, byte s) {
+			setAddr(a);
+			setSeq(s);
+		}
+
+		public byte getSeq() {
+			return seq;
+		}
+
+		public void setSeq(byte seq) {
+			this.seq = seq;
+		}
+
+		public InetAddress getAddr() {
+			return addr;
+		}
+
+		public void setAddr(InetAddress addr) {
+			this.addr = addr;
+		}		
+	}
+
+}
