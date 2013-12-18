fa521523417f0d1b48cc0c3ee22f96d27909e5f6
nicola <nicolacastaman@gmail.com>
2013-12-18 12:11:55 +0100

    Small changes

--- aside-code/Connection.java
+++ aside-code/Connection.java
@@ -12,14 +12,16 @@ public class Connection {
 
 	private static final byte[] ACK = { (byte) 0x00 };
 	private static final byte[] NACK = { (byte) 0xFF };
-	
+
 	Listener l;
+
+	private byte sn = 00000000;
 
 	public Connection() {
 		l = new Listener();
 		l.start();
 	}
-	
+
 	private class Listener extends Thread {
 
 		private DatagramSocket receiveSocket;
@@ -43,23 +45,25 @@ public class Connection {
 
 					System.out.println("receive");
 
-					byte[] data = new byte[receivePacket.getLength() - 1];
+					byte[] data = new byte[receivePacket.getLength() - 2];
 					System.arraycopy(message, 0, data, 0, data.length);
 
-					if (CRC8.calculate(data) == message[receivePacket
-							.getLength() - 1]) {
+					byte crc = message[receivePacket.getLength() - 1];
+					byte sn = message[receivePacket.getLength() - 2];
+					InetAddress address = receivePacket.getAddress();
+
+					if (CRC8.calculate(data) == crc
+							&& !Cache.isThere(address, sn)) {
 						DatagramPacket ack = new DatagramPacket(ACK,
-								ACK.length, receivePacket.getAddress(),
-								ACK_PORT);
+								ACK.length, address, ACK_PORT);
 						receiveSocket.send(ack);
 					} else {
 						DatagramPacket nack = new DatagramPacket(NACK,
-								NACK.length, receivePacket.getAddress(),
-								ACK_PORT);
+								NACK.length, address, ACK_PORT);
 						receiveSocket.send(nack);
 					}
 					for (int i = 0; i < data.length; i++) {
-						System.out.println("-> "+data[i]);
+						System.out.println("-> " + data[i]);
 					}
 
 				} catch (IOException e) {
@@ -74,9 +78,11 @@ public class Connection {
 		int retry = 0;
 
 		// message packet
-		byte[] message = new byte[data.length + 1];
+		byte[] message = new byte[data.length + 2];
 		System.arraycopy(data, 0, message, 0, data.length);
-		message[data.length] = CRC8.calculate(data);
+		message[data.length - 1] = sn; // add SN
+		message[data.length] = CRC8.calculate(message, message.length - 1); // add
+																			// CRC
 		DatagramPacket sendPacket = new DatagramPacket(message, message.length,
 				address, PORT);
 
@@ -103,6 +109,7 @@ public class Connection {
 					// MIGLIORARE VERIFICA
 					if (ackPacket.getData()[0] == ACK[0]) {
 						System.out.println("<- ACK");
+						sn++;
 						break;
 					} else {
 						System.out.println("<- NACK");
