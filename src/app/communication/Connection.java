6a51b8be7d33bfab60d3eead2314883e27199a6f
nicola <nicolacastaman@gmail.com>
2014-03-12 16:48:12 +0100

    Connection: delay fix

--- src/app/communication/Connection.java
+++ src/app/communication/Connection.java
@@ -9,7 +9,7 @@ import app.communication.CRC8;
 public class Connection {
 
 	private static final int RETRY_LIMIT = 10;
-	private static final int DELAY_TOLLERANCE = 50; //25
+	private static final int DELAY_TOLLERANCE = 25;
 	private static final int PORT = 6666;
 	private static final int BUFFER_SIZE = 20;
 	// private static final int ACK_PORT = 7777;
@@ -132,7 +132,7 @@ public class Connection {
 				} catch (SocketTimeoutException e) {
 					if (++retry >= RETRY_LIMIT) {
 						System.err.println("Max retry limit reached.");
-						Debug.output(Byte.toString(data[0]));
+						Debug.output("Flag: "+Byte.toString(data[0]));
 						sent = false;
 						break;
 					}
